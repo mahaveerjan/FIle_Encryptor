000341e6241301d9b251e16effff5811b2f8bb26
mahaveerjan <mahaveerjanagouda2002@gmail.com>
2024-10-23 18:38:30 +0530

    today first part completedgit add . hurry! parallel processing should be added next

--- src/app/processes/Task.hpp
+++ src/app/processes/Task.hpp
@@ -1,51 +1,47 @@
 #ifndef TASK_HPP
 #define TASK_HPP
 
-#include <string>
-#include <iostream>
 #include "../file_handling/IO.hpp"
+#include <fstream>
+#include <string>
 #include <sstream>
-enum class Action
-{
+
+enum class Action {
     ENCRYPT,
     DECRYPT
 };
-struct Task
-{
+
+struct Task {
     std::string filePath;
     std::fstream f_stream;
     Action action;
 
-    Task(std::fstream &&stream, Action act, std::string filePath): f_stream(std::move(stream)), action(act), filePath(filePath){}
+    Task(std::fstream&& stream, Action act, std::string filePath) : f_stream(std::move(stream)), action(act), filePath(filePath) {}
 
-    std:: string toString(){
+    std::string toString() const {
         std::ostringstream oss;
-        oss<<filePath<<","<<(action == Action::ENCRYPT? "ENCRYPT": "DECRYPT");
-        //test.txt ENCRYPT f_stream
-        //
+        oss << filePath << "," << (action == Action::ENCRYPT ? "ENCRYPT" : "DECRYPT");
         return oss.str();
     }
 
-    static Task fromString(const std:: string &taskData){
+    static Task fromString(const std::string& taskData) {
         std::istringstream iss(taskData);
         std::string filePath;
         std::string actionStr;
 
-
-        if(std::getline(iss, filePath, ',') && std::getline(iss,actionStr)){
-           Action action = (actionStr=="ENCRYPT"? Action::ENCRYPT: Action::DECRYPT);
-           IO io(filePath);
-           std::fstream f_stream = std::move(io.getFileStream());
-           if(f_stream.is_open()){
-            return Task(std::move(f_stream), action, filePath);
-           }else{
-                throw std::runtime_error("Failed to open file"+ filePath);
-           }
-        }else{
+        if (std::getline(iss, filePath, ',') && std::getline(iss, actionStr)) {
+            Action action = (actionStr == "ENCRYPT") ? Action::ENCRYPT : Action::DECRYPT;
+            IO io(filePath);
+            std::fstream f_stream = std::move(io.getFileStream());
+            if (f_stream.is_open()) {
+                return Task(std::move(f_stream), action, filePath);
+            } else {
+                throw std::runtime_error("Failed to open file: " + filePath);
+            }
+        } else {
             throw std::runtime_error("Invalid task data format");
         }
     }
 };
-
 
 #endif
